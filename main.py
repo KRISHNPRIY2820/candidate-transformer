14dd2bc78de6be5a05cfabf31b4883b8b1457cac
PRIYANSHI AGRAWAL <138807694+KRISHNPRIY2820@users.noreply.github.com>
2026-06-30 22:19:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ import json
 from src.extractors.ats_parser import parse_ats
 from src.extractors.csv_parser import parse_csv
 from src.extractors.notes_parser import parse_notes
-# from src.extractors.resume_parser import parse_resume
 from src.extractors.resume_parser import parse_resume
+
 from src.core.merger import merge_records
 from src.core.validator import validate_profile
 
@@ -16,20 +16,10 @@ def main():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument(
-        "--ats"
-    )
-
-    parser.add_argument(
-        "--csv"
-    )
-
-    parser.add_argument(
-        "--notes"
-    )
-    parser.add_argument(
-    "--resume"
-    )
+    parser.add_argument("--ats")
+    parser.add_argument("--csv")
+    parser.add_argument("--notes")
+    parser.add_argument("--resume")
 
     parser.add_argument(
         "--config",
@@ -41,31 +31,20 @@ def main():
     records = []
 
     if args.ats:
-        records.append(
-            parse_ats(args.ats)
-        )
+        records.append(parse_ats(args.ats))
 
     if args.csv:
-        records.append(
-            parse_csv(args.csv)
-        )
+        records.append(parse_csv(args.csv))
 
     if args.notes:
-        records.append(
-            parse_notes(args.notes)
-        )
+        records.append(parse_notes(args.notes))
+
     if args.resume:
-        records.append(
-            parse_resume(args.resume)
-        )
+        records.append(parse_resume(args.resume))
 
-    profile = merge_records(
-        records
-    )
+    profile = merge_records(records)
 
-    validate_profile(
-        profile
-    )
+    validate_profile(profile)
 
     output = project(
         profile,
